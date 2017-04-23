5874ff232f6522f2aae2adc5ded809a23764c64b
Dinesh Karpe <contact@dineshkarpe.me>
2017-04-23 17:04:09 +0530

    Dash UI Done here

--- app/src/main/java/ashtapradhan/com/parkme/Dashboard.java
+++ app/src/main/java/ashtapradhan/com/parkme/Dashboard.java
@@ -1,22 +1,55 @@
 package ashtapradhan.com.parkme;
 
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class Dashboard extends AppCompatActivity {
+import ashtapradhan.com.parkme.event.OnFragmentInteractionListener;
+import ashtapradhan.com.parkme.ui.ParkInOut;
+
+public class Dashboard extends AppCompatActivity implements OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        toolbar.setLogo(R.drawable.ic_toolbar);
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView)
+                findViewById(R.id.navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener
+                (new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        Fragment selectedFragment = null;
+                        switch (item.getItemId()) {
+                            case R.id.dashboard:
+                                selectedFragment = ashtapradhan.com.parkme.ui.Dashboard.newInstance();
+                                break;
+                            case R.id.parkin:
+                                selectedFragment = ParkInOut.newInstance();
+                                break;
+                        }
+                        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                        transaction.replace(R.id.frame_layout, selectedFragment);
+                        transaction.commit();
+                        return true;
+                    }
+                });
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_layout, ashtapradhan.com.parkme.ui.Dashboard.newInstance());
+        transaction.commit();
 
 
     }
@@ -41,5 +74,10 @@ public class Dashboard extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
---
+++ app/src/main/java/ashtapradhan/com/parkme/event/OnFragmentInteractionListener.java
@@ -0,0 +1,11 @@
+package ashtapradhan.com.parkme.event;
+
+import android.net.Uri;
+
+/**
+ * Created by TechSutra on 4/23/17.
+ */
+
+public interface OnFragmentInteractionListener {
+    void onFragmentInteraction(Uri uri);
+}
